ed37b70c2121a82f6e2752789ea8e1c0a305293a
Wilson Vasquez <wilsonraymundov@gmail.com>
2023-01-17 23:12:46 -0600

    Se crea Metodo get y post en API

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -3,19 +3,35 @@ import {getConnection} from "./../database/database";
 const getLenguaje =  async (req, res) => {
      try {
         const connection = await getConnection();
-    const result = await connection.query("SELECT * FROM Lenguajes");
-    res.json(result);
+      const result = await connection.query("SELECT * FROM Lenguajes");
+      res.json(result);
         
      } catch (error) {
         res.status(500);
         res.send(error.message)
         
      }
+};
 
-    
+const addLenguaje =  async (req, res) => {
+    try {
+        const {Lenguaje, Ranking} = req.body;
+        const Len = { Lenguaje, Ranking}
+
+     const connection = await getConnection();
+     const result = await connection.query("INSERT INTO Lenguajes SET ? ", Len);
+     res.json(result);
+       
+    } catch (error) {
+       res.status(500);
+       res.send(error.message)
+       
+    }
 };
 
 
+
 export const methods = {
-    getLenguaje
+    getLenguaje,
+    addLenguaje
 };
--- src/routes/languaje.routes.js
+++ src/routes/languaje.routes.js
@@ -4,5 +4,6 @@ import { methods as Controlador }  from "../controllers/controller";
 const router = Router();
 
 router.get("/api/lenguajes", Controlador.getLenguaje);
+router.post("/api/lenguajes", Controlador.addLenguaje);
 
 export default router;
